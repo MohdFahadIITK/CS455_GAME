04dc49b7ee48586b6146c3ba8b07497b9acdc2da
MayankPushpjeet <100858122+MayankPushpjeet@users.noreply.github.com>
2024-08-12 02:33:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<LandingPage />} />
+        <Route path='/CS455_GAME/' element={<LandingPage />} />
       </Routes>
     </Router>
   );
