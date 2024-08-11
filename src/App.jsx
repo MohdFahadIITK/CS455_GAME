b911765d750025eedfd591f112d2b472ec1854cd
= <mayank.pushpjeet@astrotalk.com>
2024-08-12 03:39:58 +0530

    Router_Bug_Fix_03

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Link,Outlet } from 'react-router-dom';
 import './App.css';
 import LandingPage from './Components/LandingPage';
 import EndingPageLoss from './Components/EndingPageLoss';
@@ -8,13 +8,9 @@ import EndingPageWin from './Components/EndingPageWin';
 function App() {
   
   return (
-    <BrowserRouter basename="/CS455_GAME">
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/L' element={<EndingPageLoss/>}/>
-        <Route path='/W' element={<EndingPageWin/>}/>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Outlet/>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import LandingPage from './Components/LandingPage.jsx'
+import EndingPageLoss from './Components/EndingPageLoss.jsx'
+
+const router = createBrowserRouter([
+  {
+    path : "/CS455_GAME/",
+    element : <App/>,
+    children : [
+      {
+        path : "/CS455_GAME/",
+        element : <LandingPage/>
+      },
+      {
+        path : "/CS455_GAME/L",
+        element : <EndingPageLoss/>
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
