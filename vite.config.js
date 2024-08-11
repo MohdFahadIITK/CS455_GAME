bdaa528d7bc15377f1f453adf108f1106d9f3b5c
MayankPushpjeet <100858122+MayankPushpjeet@users.noreply.github.com>
2024-08-12 02:21:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Set the base URL for GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/CS455_GAME/', // Use the correct base URL for GitHub Pages
+  base: '/', // Use the correct base URL for GitHub Pages
 })
