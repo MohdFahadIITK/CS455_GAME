36acd86f1d1f4053836d1d5b77539c7bc42573e2
= <mayank.pushpjeet@astrotalk.com>
2024-08-12 02:56:00 +0530

    Bug_Resolve

--- src/Components/TryAgainButton.jsx
+++ src/Components/TryAgainButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export default function PlayButton() {
+export default function TryAgainButton() {
   const navigate = useNavigate();
 
   const handleClick = () => {
