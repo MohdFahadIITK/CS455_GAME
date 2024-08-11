bb59de5c84d0e6111bf845dabc21f2fd71a94562
Mohd Fahad <88942913+MohdFahadIITK@users.noreply.github.com>
2024-08-11 19:54:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace '/CS455_GAME/' with the correct base URL
 export default defineConfig({
   plugins: [react()],
-  base: '/CS455_GAME/', // Set the base URL for GitHub Pages
+  base: '', // Set the base URL for GitHub Pages
 })
