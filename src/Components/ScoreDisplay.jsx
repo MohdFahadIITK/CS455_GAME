96d7b8535a7d3024758b11a9d5f5b6ad459b16ee
MohdFahadIITK <fahadm20@iitk.ac.in>
2024-08-12 17:14:12 +0530

    made scoreboard responsive

--- src/Components/ScoreDisplay.jsx
+++ src/Components/ScoreDisplay.jsx
@@ -4,14 +4,12 @@ export default function ScoreDisplay({score, balls}) {
     const overs =  Math.floor(balls/6);
     const remainingBValls = balls % 6;
     return (
-        <div className="flex justify-center space-x-4 items-center px-4 py-4  h-1/2 w-full">
-            <div className="flex flex-col items-center justify-center">
-                <div className="flex items-center justify-center bg-white h-16 w-60 text-3xl border-2 border-black border-solid rounded-md">{score}</div>
-                <div className="flex mt-2 font-semibold text-2xl">Score</div>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-                <div className="flex items-center justify-center bg-white h-16 w-60 text-3xl border-2 border-black border-solid rounded-md">{overs}.{remainingBValls}</div>
-                <div className="flex mt-2 font-semibold text-2xl">Overs</div>
+        <div className="flex justify-center items-center px-10 py-10 h-1/2 w-full">
+            <div className="flex flex-col items-center justify-center w-full py-10">
+                <div className="flex items-center justify-center bg-white h-16 w-full text-3xl border-2 border-black border-solid rounded-md">{score}</div>
+                <div className="flex items-center justify-center bg-white h-16 w-full text-3xl ">Score</div>
+                <div className="flex items-center justify-center bg-white h-16 w-full text-3xl border-2 border-black border-solid rounded-md">{overs}.{remainingBValls}</div>
+                <div className="flex items-center justify-center bg-white h-16 w-full text-3xl ">Over</div>
             </div>
         </div>
     );
