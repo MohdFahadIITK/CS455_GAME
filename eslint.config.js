85620fe90fd2f6721798350338b61fcbb96747a4
MohdFahadIITK <fahadm20@iitk.ac.in>
2024-09-13 05:20:39 +0530

    Linter added successfully

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,26 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
 
 export default [
   {
-    files: ['**/*.{js,jsx}'],
-    ignores: ['dist'],
+    plugins: {
+      import: importPlugin,
+    },
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.mocha,
       },
     },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "import/order": ["error", { "groups": [["builtin", "external", "internal"]] }],
+      "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+      "no-duplicate-imports": "error",
+      "no-var": "error",
+      "func-style": ["error", "declaration", { "allowArrowFunctions": true }],
+      "semi": ["error", "always"],
     },
   },
-]
+  pluginJs.configs.recommended,
+];
