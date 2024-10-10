f121105d49932805ab41cb3edcb95d8dfc3da1e8
= <mayank.pushpjeet@astrotalk.com>
2024-10-11 02:12:19 +0530

    created router to route all reqest comming to prefix /app to respective end points

---
+++ backend/src/routes.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import {signup, signin} from "./auth";
+import {getSessionScore, getTop10Scores, saveSessionScore} from "./score";
+
+
+const router = express.Router();
+
+router.post('/signup', async (req: express.Request, res: express.Response) => {
+    await signup(req, res);
+});
+
+router.post('/signin', async (req: express.Request, res: express.Response) => {
+     await signin(req, res);
+});
+
+router.get('/get-current-score', async (req: express.Request, res: express.Response)=>{
+    await getSessionScore(req,res);
+})
+
+router.post('/save-current-score', async (req: express.Request, res: express.Response)=>{
+    await saveSessionScore(req,res);
+})
+
+router.get('/get-top-scores',async (req: express.Request, res: express.Response)=>{
+    await getTop10Scores(req,res);
+})
+
+
+
+export default router;
